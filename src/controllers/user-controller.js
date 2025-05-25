3fee090cb449953d294e411d3fb0fe8abbbd4162
nixena <nikanozadze1010@gmail.com>
2025-05-25 11:38:13 +0400

    feat: add put & delete request

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -1,5 +1,6 @@
 import User from "../models/User.js";
 import addUserSchema from "../schemas/add-user-schema.js";
+import updateUserSchema from "../schemas/update-user-schema.js";
 
 export const getAllUsers = async (req, res) => {
   try {
@@ -32,6 +33,51 @@ export const createUser = async (req, res) => {
     console.error(error, "this error");
     return res.status(500).json({
       message: "An error occurred while creating the user",
+    });
+  }
+};
+
+export const updateUser = async (req, res) => {
+  const { body, params } = req;
+  const id = params.id;
+  try {
+    const user = await User.findOne({ id });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    const validator = updateUserSchema();
+    const { error, value } = validator.validate(body);
+    if (error) {
+      return res.status(400).json({ message: error.details });
+    }
+    const { email } = value;
+
+    user.email = email;
+    await user.save();
+    return res.status(200).json(user);
+  } catch (error) {
+    console.error(error, "this error");
+    return res.status(500).json({
+      message: "An error occurred while updating the user",
+    });
+  }
+};
+
+export const deleteUser = async (req, res) => {
+  const { params } = req;
+  const id = params.id;
+
+  try {
+    const user = await User.findOne({ id });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    await user.deleteOne();
+    return res.status(200).json({ message: "User deleted successfully" });
+  } catch (error) {
+    console.error(error, "this error");
+    return res.status(500).json({
+      message: "An error occurred while deleting the user",
     });
   }
 };
